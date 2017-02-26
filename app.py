d70a38932242cfaa9f2d3a78760c06084cae0c01
Brendon <brennon.mckeever@gmail.com>
2017-02-26 17:57:18 -0600

    need to work with JSON for tour

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func, exists, and_
 import itertools
+import json
+import requests
+import sys
+import urllib.request
 from datetime import *
 from bandPhotoAPI import BandPhotoAPI
 from photo import Photo
 from tour_processing import *
 
 google_key = 'AIzaSyAobnniB69jdRIJpi--eltsM1Z6tcPkqt4' # We will need to secure this later.
-#TM_key = 'xqbpUW8lmN8nnoX3UHO7suHosVMf8oBF'
+TM_key = 'xqbpUW8lmN8nnoX3UHO7suHosVMf8oBF'
 
 
 
@@ -21,14 +25,27 @@ def home_page():
 
     #addPhotoToWebApp() TODO this will need to be tested, SSL fail.
 
-    get_band_schedule()
+    #get_band_schedule()
+
+    band = "lil wayne"
+
+    url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey={}&keyword={}".format(TM_key, band)
+    print(url)
+    request_url = requests.get(url)
+    json_return = request_url.json()
+    json_1 = json.dumps(json_return)
+    working = json.loads(json_1)
+    json_value = working[0]
+    print(json_value['events'])
+
+
 
     if request.method == 'POST':
 
         return render_template('home_page.html', key = google_key, place = request.form['venue'], state = request.form['state'])
 
 
-    return render_template('home_page.html', key = google_key, place = "guthrie+theater", state = "MN")
+    return render_template('home_page.html', key = google_key, place = "guthrie+theater", state = "MN", tour_key = TM_key, band_name = "lil wayne")
 
 
 
