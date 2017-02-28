94fd0c2d58dbb1efe222fa89f8beda89a973f30c
Brendon <brennon.mckeever@gmail.com>
2017-02-27 20:09:46 -0600

    I'm still having issues iterating over a large and complex json.

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from data_processing import *
 
 google_key = 'AIzaSyAobnniB69jdRIJpi--eltsM1Z6tcPkqt4' # We will need to secure this later.
 TM_key = 'xqbpUW8lmN8nnoX3UHO7suHosVMf8oBF'
-
+url_test = ''
 
 app = Flask(__name__)
 
@@ -33,6 +33,7 @@ def home_page():
             ]
 
         return_list = concurrent_requests(URLS)
+        print(return_list)
 
         return render_template('home_page.html', key = google_key, place = return_list[1], state = request.form['state'])
 
@@ -56,23 +57,26 @@ def addPhotoToWebApp():# change pearl jam to a variable for whichever band the u
 
 
 
+def setup():
 
-def get_band_tour_info(key, band):
+    url_test = addPhotoToWebApp()
 
-    url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey={}&keyword={}".format(key, band)
-    print(url)
-    request_url = requests.get(url)
-    json_return = request_url.json() # TODO need to get specific information from here.
 
-    print(type(json_return))
-    artist = json_return["_embedded"]['events'] # Gets Artist
-    print(type(artist))
-    print(len(artist))
-    # venue = artist["_embedded"]['venues'] # Gets venue
-    # print(type(venue))
-    # print(len(venue))
+# def get_band_tour_info(key, band):
+#
+#     url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey={}&keyword={}".format(key, band)
+#     print(url)
+#     request_url = requests.get(url)
+#     json_return = request_url.json() # TODO need to get specific information from here.
+#
+#     print(type(json_return))
+#     artist = json_return["_embedded"]['events'] # Gets Artist
+#     print(type(artist))
+#     print(len(artist))
+#     # venue = artist["_embedded"]['venues'] # Gets venue
+#     # print(type(venue))
+#     # print(len(venue))
 
 
 if __name__ == '__main__':
     app.run(debug = True)
-    addPhotoToWebApp()
--- data_processing.py
+++ data_processing.py
@@ -16,7 +16,6 @@ def load_url(url, timeout):
 
 def concurrent_requests(url_list):
 
-    info_list = []
     # We can use a with statement to ensure threads are cleaned up promptly
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
         # Start the load operations and mark each future with its URL
@@ -29,22 +28,41 @@ def concurrent_requests(url_list):
                 request_url = requests.get(url)
                 json_return = request_url.json() # TODO need to get specific information from here.
 
+                url_return = handle_tour_json(json_return)
+
+                return url_return
+
+
             except Exception as exc:
                 print('%r generated an exception: %s' % (url, exc))
-            else:
-                #print('%r page is %d bytes' % (url, len(data)))
-                json_return = request_url.json() # TODO need to get specific information from here.
 
-                print(type(json_return))
-                artist = json_return["_embedded"]['events'][0] # Gets Artist
-                print(artist['name'])
-                # print(type(artist))
-                # print(len(artist))
-                venue = artist["_embedded"]['venues'][0] # Gets venue
-                print(venue['name'])
-                info_list.append(artist['name'])
-                info_list.append(venue['name'])
-                # print(type(venue))
-                # print(len(venue))
+
+def handle_tour_json(json_dict):
+
+    info_list = []
+
+    try:
+
+
+        # I want this to loop over the whole json and return a record if it finds MN as the
+        # stateCode, its not working.
+        states = [state["name"] for state in json_dict["_embedded"]['events']
+                    if state[0]["_embedded"]['venues']["state"]["stateCode"] == "IL"]
+
+        print(states)
+
+
+
+    except Exception as e:
+
+        print("problem", e)
+
+
+    artist = json_dict["_embedded"]['events'][0] # Gets Artist
+    print(artist['name'])
+    venue = artist["_embedded"]['venues'][0] # Gets venue
+    print(venue['name'])
+    info_list.append(artist['name'])
+    info_list.append(venue['name'])
 
     return info_list
