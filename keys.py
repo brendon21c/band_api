6540f768b8b82aa2df9db25063e8dfdb0ed577e3
marie8bit <yd7581ku@go.minneapolis.edu>
2017-03-02 14:22:34 -0600

    uses API to get photo URLs

--- keys.py
+++ keys.py
@@ -1,4 +1,5 @@
 keys = {
 'TM_KEY' : 'xqbpUW8lmN8nnoX3UHO7suHosVMf8oBF',
-'GOOGLE_KEY' : 'AIzaSyAobnniB69jdRIJpi--eltsM1Z6tcPkqt4'
+'GOOGLE_KEY' : 'AIzaSyAobnniB69jdRIJpi--eltsM1Z6tcPkqt4',
+'FLICKR KEY' : '2fbb027bbfccb9d725d74f2391a6f525'
 }
