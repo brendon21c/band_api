7faf8f99294ffb30faf6f8488eea196d2c9023bb
Brendon <brennon.mckeever@gmail.com>
2017-03-05 10:23:08 -0600

    added some code to lyrics

--- lyrics.py
+++ lyrics.py
@@ -1,7 +1,7 @@
 from keys import keys
 from get_data import *
-import musixmatch.api
-import musixmatch
+# import musixmatch.api
+# import musixmatch
 import urllib.request
 import requests
 import json
@@ -10,25 +10,33 @@ import json
 #2. you send the track id to MM to return lyrics
 #3. MM will return the lyrics to the song of the track_id
 #Documentation found here: https://developer.musixmatch.com/documentation
-def get_track_id(trackID):
+def get_track_id(song):
     key = keys['MUSIXMATCH KEY']
-    trackName = get_data_for_band(song)
-    musixmatchSEARCH = 'http://api.musixmatch.com/ws/1.1/track.search?q_track={}&format=json'.format(trackName)
+    #trackName = get_data_for_band(song)
+    musixmatchSEARCH = 'http://api.musixmatch.com/ws/1.1/track.search?q_track={}'.format(song)
+    print(musixmatchSEARCH)
     #trackName should be a song title
     #this will return a JSON
+    request = requests.get(musixmatchSEARCH)
+    search_json = request.json()
+    print(search_json)
 
-    trackID = musixmatchSEARCH.'body.track_list.track.track_id'
+    #trackID = musixmatchSEARCH.'body.track_list.track.track_id'
+    trackID = search_json['body']['track_list'][0]
+    print(trackID)
+
     #need the trackID to send to musixmatch in order to get lyrics
 
     return trackID
 
-def get_lyrics_for_band(band):
+def get_lyrics_for_band(song):
+
     key = keys['MUSIXMATCH KEY']
-    trackID = get_track_id()
 
-    musicmatchREQUESTLYRICS =
-    'http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id={}&format=json'.format(trackID)
+    trackID = get_track_id(song)
 
-    lyrics = musicmatchREQUESTLYRICS.'body.lyrics.lyrics_body'
+    musicmatchREQUESTLYRICS = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id={}&format=json'.format(trackID)
 
-  return lyrics
+    #lyrics = musicmatchREQUESTLYRICS.'body.lyrics.lyrics_body'
+
+    return lyrics
