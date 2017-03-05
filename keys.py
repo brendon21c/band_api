46243b613bdab4f24c2113bb44780b57184685aa
Jelly Beans <Jelly@Beans.com>
2017-03-05 09:16:07 -0600

    Added Lyrics API

--- keys.py
+++ keys.py
@@ -2,4 +2,5 @@ keys = {
 'TM_KEY' : 'xqbpUW8lmN8nnoX3UHO7suHosVMf8oBF',
 'GOOGLE_KEY' : 'AIzaSyAobnniB69jdRIJpi--eltsM1Z6tcPkqt4',
 'FLICKR KEY' : '2fbb027bbfccb9d725d74f2391a6f525'
+'MUSIXMATCH KEY' : '01b12f9088f82da4fcf7e5c3453c3573'
 }
--- lyrics.py
+++ lyrics.py
@@ -1,7 +1,30 @@
+from keys import keys
+import musixmatch.api
+import musixmatch
+import urllib.request
+import requests
+import json
+
+#1. you have to get a track id
+#2. you send the track id to MM to return lyrics
+#3. MM will return the lyrics to the song of the track_id
+#Documentation found here: https://developer.musixmatch.com/documentation
+def get_track_id(trackID):
+    key = keys['MUSIXMATCH KEY']
+    musixmatchSEARCH = 'http://api.musixmatch.com/ws/1.1/track.search?q_track={}&format=json'.format(trackName)
+    #this will return a JSON
+    trackID = 'body.track_list.track.track_id'
+
+    return trackID
 
 def get_lyrics_for_band(band):
-    pass
+    key = keys['MUSIXMATCH KEY']
+    musicmatchREQUESTLYRICS =
+    'http://api.musixmatch.com/ws/1.1/track.search?q_track={}&format=json'.format(trackName)
 
-    # Get lyrics for band, return.
-    # https://developer.musixmatch.com/
-    # other APIs available
+  return musicmatchREQUEST
+
+
+
+except KeyError:
+    pass
